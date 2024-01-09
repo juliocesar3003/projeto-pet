8bd507074b7e8933ab03be10f93debeccffe3b67
julio cesar <julio.cesar.roseli@gmail.com>
2024-01-09 19:46:12 -0300

    funções de crud feitas na classe resouce e endpoints ja testados no postman

--- src/main/java/com/projeto/api/entidades/Clientes.java
+++ src/main/java/com/projeto/api/entidades/Clientes.java
@@ -30,7 +30,7 @@ private static final long serialVersionUID = 1L;
 
 	 private String nome;
 	
-	 private Integer celular;
+	 private Long celular;
 	
 	 private String endereco;
 
--- src/main/java/com/projeto/api/resource/ClienteResource.java
+++ src/main/java/com/projeto/api/resource/ClienteResource.java
@@ -1,5 +1,66 @@
 package com.projeto.api.resource;
 
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.service.annotation.DeleteExchange;
+import org.springframework.web.service.annotation.PutExchange;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.projeto.api.entidades.Clientes;
+import com.projeto.api.service.ClientesService;
+
+import jakarta.annotation.Resource;
+
+@RestController
+@RequestMapping(value = "/users")
 public class ClienteResource {
 
+	@Autowired
+	private ClientesService service;
+	
+	@GetMapping
+	public ResponseEntity<List<Clientes>> findAll(){
+		List<Clientes> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Clientes> findbyId(@PathVariable Long id ){
+		Clientes obj = service.findByid(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Clientes> insert(@RequestBody Clientes obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
+	
+	@DeleteMapping(value = "{/id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value = " {/id}")
+	public ResponseEntity<Usuario> update(@PathVariable Long id, @RequestBody Clientes obj){
+		obj = service.update(id, obj);
+		return ResponseEntity.ok().body(obj);
+	}
 }
+
+
+
